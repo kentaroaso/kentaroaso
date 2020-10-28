81ec6264d9b1b58398a9f415f95f08966a25294c
kentaroaso <63499982+kentaroaso@users.noreply.github.com>
2020-10-28 19:43:01 +0900

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-const startButton = document.getElementById('start-btn')
+  const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
@@ -85,12 +85,228 @@ const questions = [
     ]
   },
   {
+    question: 'ついてきてください',
+    answers: [
+      { text: 'follow me', correct: true },
+      { text: 'this way', correct: false }
+    ]
+  },
+  {
+    question: '注目しろ',
+    answers: [
+      { text: 'attention', correct: true },
+      { text: 'get back', correct: false }
+    ]
+  },
+  {
+    question: '道を空けろ',
+    answers: [
+      { text: 'clear the way', correct: true },
+      { text: 'make it way', correct: false }
+    ]
+  },
+  {
+    question: '直ちに解散しろ',
+    answers: [
+      { text: 'disperse now', correct: true },
+      { text: 'good bye' , correct: false }
+    ]
+  },
+  {
+    question: '大丈夫か',
+    answers: [
+      { text: 'are you all rigth', correct: true },
+      { text: 'are you OK', correct: false }
+    ]
+  },
+  {
+    question: '足を骨折しました',
+    answers: [
+      { text: 'I broke my leg', correct: true },
+      { text: 'he broken', correct: false }
+    ]
+  },
+  {
+    question: '意識がありません',
+    answers: [
+      { text: 'He is unconscious', correct: true },
+      { text: 'I am conscious', correct: false }
+    ]
+  },
+  {
+    question: 'AEDを持ってきてください',
+    answers: [
+      { text: 'bring an AED please', correct: true },
+      { text: 'give me AED', correct: false }
+    ]
+  },
+  {
+    question: '救急車をよんでください',
+    answers: [
+      { text: 'please call an ambulanse', correct: true },
+      { text: '', correct: false }
+    ]
+  },
+  {
+    question: '私は日本隊のあそうです。お会いできて嬉しいです',
+    answers: [
+      { text: 'I am sag aso of japanese unit nice to meet you', correct: true },
+      { text: 'hi nice to meet you', correct: false }
+    ]
+  },
+  {
+    question: 'お名前を伺っても良いですか',
+    answers: [
+      { text: 'may I have your name', correct: true },
+      { text: 'can I call your name', correct: false }
+    ]
+  },
+  {
+    question: 'もう一度おっしゃってください',
+    answers: [
+      { text: 'please say that again', correct: true },
+      { text: 'one more please', correct: false }
+    ]
+  },
+  {
+    question: '少しお待ちください',
+    answers: [
+      { text: 'please wait a minute', correct: true },
+      { text: 'just it moment', correct: false }
+    ]
+  },
+  {
+    question: '私に協力してください',
+    answers: [
+      { text: 'plesse cooperate with me', correct: true },
+      { text: 'power for me', correct: false }
+    ]
+  },
+  {
+    question: 'もっとゆっくり話していただけませんか',
+    answers: [
+      { text: 'please speak more slowly', correct: true },
+      { text: 'down speak', correct: false }
+    ]
+  },
+  {
+    question: '通訳を連れてきます',
+    answers: [
+      { text: 'I will bring an interpreter', correct: true },
+      { text: 'import interpreter ', correct: false }
+    ]
+  },
+  {
+    question: '理解できません',
+    answers: [
+      { text: 'I could not understand', correct: true },
+      { text: 'no way', correct: false }
+    ]
+  },
+  {
+    question: 'ここに書いていただけますか',
+    answers: [
+      { text: 'please write it down', correct: true },
+      { text: 'write this', correct: false }
+    ]
+  },
+  {
+    question: '名前を書いていただけますか',
+    answers: [
+      { text: 'please write down your name', correct: true },
+      { text: 'please write it down', correct: false }
+    ]
+  },
+  {
+    question: '通訳を呼びます',
+    answers: [
+      { text: 'I will call interpreter', correct: true },
+      { text: 'I will do call interpreter', correct: false }
+    ]
+  },
+  {
+    question: 'お手洗いはどこですか',
+    answers: [
+      { text: 'where is a rest room', correct: true },
+      { text: 'where is a WC', correct: false }
+    ]
+  },
+  {
+    question: 'キャンプ富士への行き方を教えてください',
+    answers: [
+      { text: 'please tell me how to ge to camp FUji', correct: true },
+      { text: 'where is a Fuji', correct: false }
+    ]
+  },
+  {
+    question: 'これは警告だ',
+    answers: [
+      { text: 'this is the warning', correct: true },
+      { text: 'this final warning', correct: false }
+    ]
+  },
+  {
+    question: '止まれ',
+    answers: [
+      { text: 'stop', correct: true },
+      { text: 'do not move', correct: false }
+    ]
+  },
+  {
+    question: '動くな',
+    answers: [
+      { text: 'do not move', correct: true },
+      { text: 'freeze', correct: false }
+    ]
+  },
+  {
+    question: '下がれ',
+    answers: [
+      { text: 'stand back', correct: true },
+      { text: 'take off me', correct: false }
+    ]
+  },
+  {
+    question: 'その行為をやめろ',
+    answers: [
+      { text: 'stop it', correct: true },
+      { text: 'do not it', correct: false }
+    ]
+  },
+  {
+    question: 'ここに入るな',
+    answers: [
+      { text: 'do not enter', correct: true },
+      { text: 'keep away', correct: false }
+    ]
+  },
+  {
+    question: '近づくな',
+    answers: [
+      { text: 'keep away', correct: true },
+      { text: 'get off me', correct: false }
+    ]
+  },
+  {
+    question: '2歩前へ',
+    answers: [
+      { text: 'tow steps foward ', correct: true },
+      { text: 'tow step back', correct: false }
+    ]
+  },
+  {
+    question: '',
+    answers: [
+      { text: '', correct: true },
+      { text: '', correct: false }
+    ]
+  },
+
+  {
     question: '撃つぞ',
     answers: [
-      { text: 'I will do fire', correct: true },
-      { text: 'I do fire', correct: false },
+      { text: 'I am gonna shoot you', correct: true },
       { text: 'I will fire', correct: false },
-      { text: 'Stop or I will fire', correct: false }
     ]
   },
   {
