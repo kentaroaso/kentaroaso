d2f8440417bf128f4976059800b32aa119c40d06
kentaroaso <63499982+kentaroaso@users.noreply.github.com>
2021-01-10 16:28:37 +0900

    Update bgswitcher.js

--- bgswitcher.js
+++ bgswitcher.js
@@ -570,7 +570,7 @@ jQuery(function($) {
       'https://kentaroaso.github.io/kentaroaso/run.jpg',
       'https://kentaroaso.github.io/kentaroaso/shoes.jpg'], // 切り替える背景画像
     Interval: 5000, //切り替えの間隔 1000=1秒
-    start: false, //$.fn.bgswitcher(config)をコールした時に切り替えを開始
+    start: true, //$.fn.bgswitcher(config)をコールした時に切り替えを開始
     loop: true, //切り替えをループする
     shuffle: true, //背景画像の順番をシャッフルする
     effect: "fade", //エフェクトの種類 (fade / blind / clip / slide / drop / hide)
