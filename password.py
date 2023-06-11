e23d6db2870a411dcb144c9a2341feb786e5cc2e
kentaroaso <63499982+kentaroaso@users.noreply.github.com>
2023-06-11 19:59:36 +0900

    Delete password.py

--- password.py
+++
@@ -1,6 +0,0 @@
-import random
-
-passlen = int(input("パスワードの文字数を入力してください。 : "))
-word_select = "1234567890-^¥qwertyuiop@[asdfghjkl;:]zxcvbnm,./_!#$%&'()=~|`{+*}<>?"
-enter_password = "".join(random.sample(word_select, passlen))
-print(enter_password)
